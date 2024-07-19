9ac6417b86f810541791f41ad0df91dfe38f8088
AllenLiao <allen10153@gmail.com>
2024-07-19 17:36:03 +0800

    router init

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// create restaurant
+app.get('/restaurants/new', (req, res) => {
+  res.send('create page.')
+})
+
+app.post('/restaurants/', (req, res) => {
+  res.send('restaurant has been created.')
+})
+
+// read restaurant
+app.get('/restaurants', (req, res) => {
+  res.send('read all restaurants.')
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  res.send(`read restaurant: ${req.params.id}.`)
+})
+
+// update restaurant
+app.get('/restaurants/:id/edit', (req, res) => {
+  res.send(`restaurant: ${req.params.id} edit page.`)
+})
+
+app.put('/restaurants/:id', (req, res) => {
+  res.send(`restaurant: ${req.params.id} has been modified.`)
+})
+
+// delete restaurant
+app.delete('/restaurants/:id', (req, res) => {
+  res.send(`restaurant: ${req.params.id} has been deleted.`)
+})
+
 // ----- start to listen on port -----
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}.`)
