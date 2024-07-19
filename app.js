7f44a5d89099d51912974583eaa63cc1d5da5b0c
AllenLiao <allen10153@gmail.com>
2024-07-20 01:41:35 +0800

    implement show page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'hbs')
 app.set('views', './views')
 
 const db = require('./models')
+const { raw } = require('mysql2')
 const Restaurant = db.Restaurant
 
 // ----- define relative variables -----
@@ -27,13 +28,20 @@ app.post('/restaurants/', (req, res) => {
 
 // read restaurant
 app.get('/', (req, res) => {  // modify route "/restaurants" to "/" to take this page as index page
-  return Restaurant.findAll({ raw: true })
+  return Restaurant.findAll({
+    attributes: ['id', 'name', 'category', 'image', 'rating'],
+    raw: true })
     .then(restaurants => res.render('index', {restaurants}))
     .catch(error => res.status(422).json(error))
 })
 
 app.get('/restaurants/:id', (req, res) => {
-  res.send(`read restaurant: ${req.params.id}.`)
+  const id = req.params.id
+  return Restaurant.findByPk(id, {
+    attributes: ['id', 'name', 'category', 'image', 'location', 'google_map', 'phone', 'description'],
+    raw: true
+  })
+    .then(restaurant => res.render('show', {restaurant}))
 })
 
 // update restaurant
